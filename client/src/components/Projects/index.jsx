0e4c14e1bb3751119c61bcf51bd10068c1d8d45e
Jake <jakedavisemail@gmail.com>
2023-06-06 12:59:07 -0600

    Changing Modal design

--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -1,17 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Slider from './Slider.jsx';
-import Modal from './Modal.jsx';
-
 import projects from './projects.js';
 
 function Projects({ theme }) {
-  const [project, setProject] = useState({});
-
   return (
     <div className="page_section big_text" id="projects">
       <div className={`tiny_text section_text ${theme}`}>My work</div>
-      <Slider setProject={setProject} theme={theme} data={projects} />
-      <Modal project={project} setProject={setProject} theme={theme} />
+      <Slider theme={theme} data={projects} />
     </div>
   );
 }
