9cf683dec037872ce459e74a22684a3e6334b83f
Jake <jakedavisemail@gmail.com>
2023-06-09 11:16:16 -0600

    Button Styling

--- client/src/components/More/index.jsx
+++ client/src/components/More/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function More() {
+  const [selected, setSelected] = useState(null);
+
+  const handleClick = (target) => {
+    setSelected(target === selected ? null : target);
+  };
+
   return (
     <div className="page_section big_text" id="more">
-      <button>More About Me</button>
-      <button>Contact Me</button>
+      <button onClick={() => handleClick('b1')} className={selected === 'b1' ? 'selected' : null} id="b1">More About Me</button>
+      <button onClick={() => handleClick('b2')} className={selected === 'b2' ? 'selected' : null} id="b2">Contact Me</button>
     </div>
   );
 }
