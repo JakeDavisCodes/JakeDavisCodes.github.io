c617857e908301c7b1f2433d698f265bb8d5fe60
Jake <jakedavisemail@gmail.com>
2023-06-11 17:25:15 -0600

    Secret

--- client/src/components/More/index.jsx
+++ client/src/components/More/index.jsx
@@ -9,6 +9,7 @@ function More() {
 
   return (
     <div className="page_section big_text" id="more">
+      <div className="secret">{'Hey dont steal my contact info >:('}</div>
       <button onClick={() => handleClick('b1')} className={selected === 'b1' ? 'selected' : null} id="b1">More About Me</button>
       <button onClick={() => handleClick('b2')} className={selected === 'b2' ? 'selected' : null} id="b2">Contact Me</button>
     </div>
