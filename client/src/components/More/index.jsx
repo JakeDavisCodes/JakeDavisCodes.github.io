67d62f4f8707bf706404682b01ac2d0e20044548
Jake <jakedavisemail@gmail.com>
2023-06-12 10:54:30 -0600

    More buttons color themeatic change

--- client/src/components/More/index.jsx
+++ client/src/components/More/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function More() {
+function More({ theme }) {
   const [selected, setSelected] = useState();
 
   const handleClick = (target) => {
@@ -8,7 +8,7 @@ function More() {
   };
 
   return (
-    <div className="page_section big_text" id="more">
+    <div className={`${theme} page_section big_text`} id="more">
       <div className="secret">{'Hey dont steal my contact info >:('}</div>
       <button onClick={() => handleClick('b1')} className={selected === 'b1' ? 'selected' : null} id="b1">More About Me</button>
       <button onClick={() => handleClick('b2')} className={selected === 'b2' ? 'selected' : null} id="b2">Contact Me</button>
