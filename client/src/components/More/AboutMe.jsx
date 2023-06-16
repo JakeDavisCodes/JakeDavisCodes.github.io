15a5def02a0d48b7b773b9e5cce0f42dafe131ba
Jake <jakedavisemail@gmail.com>
2023-06-16 13:30:16 -0600

    Skills List

--- client/src/components/More/AboutMe.jsx
+++ client/src/components/More/AboutMe.jsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
 
+function Skill({ type, list }) {
+  return (
+    <div className="skill">
+      <p>
+        {type}
+      </p>
+      <small>{list.join(' | ')}</small>
+    </div>
+  );
+}
+
 function AboutMe({ position, theme }) {
   return (
     <div
@@ -9,7 +20,13 @@ function AboutMe({ position, theme }) {
         transition: 'all 0.6s ease',
       }}
     >
-      PH
+      <div>
+        <Skill type="Front-End" list={['JavaScript', 'TypeScript', 'React', 'ReactNative', 'HTML', 'CSS']} />
+        <Skill type="Back-End" list={['Node.JS', 'Express', 'PostgreSQL', 'MongoDB', 'MySQL', 'REST', 'FireBase']} />
+        <Skill type="Testing / Deployment" list={['K6', 'Loader.IO ', 'Jest', 'SuperTest', 'Mocha', 'Chai', 'AWS', 'DreamHost']} />
+        <Skill type="Developer Tools" list={['Vim', 'Git', 'NPM', 'Nx', 'Webpacl', 'Babel']} />
+        <Skill type="Methodoligies" list={['Scrum', 'Agile', 'TDD']} />
+      </div>
     </div>
   );
 }
