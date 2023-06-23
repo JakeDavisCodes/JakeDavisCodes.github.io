359e0f6b877248ef7d7ca70a84c2ce5d5b712611
Jake <jakedavisemail@gmail.com>
2023-06-23 14:34:46 -0600

    Correct Naming

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -5,8 +5,8 @@ function About({ theme }) {
   return (
     <div className="page_section big_text" id="about">
       <img
-        src="images/Me.jpeg"
-        alt="A dazzling photograph of me should be here"
+        src="images/me.jpeg"
+        alt="A dazzling photograph of me should be here..."
       />
       <div className={`tiny_text section_text ${theme}`}>About</div>
     </div>
